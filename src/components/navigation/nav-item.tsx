c5dfa448e89b9f3384283f1a98049b5b78ef5841
barteks79 <sade79dev@gmail.com>
2025-02-21 22:22:05 +0100

    feat: create nav-item component and implement left side of the navigation

---
+++ src/components/navigation/nav-item.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { type PropsWithChildren } from 'react';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+
+type NavItemProps = {
+	href: string;
+} & PropsWithChildren;
+
+export default function NavItem({ href, children }: NavItemProps) {
+	const pathname = usePathname();
+	const isActive = pathname === href;
+
+	return (
+		<li className="text-sm xl:text-base">
+			<Link
+				href={href}
+				className={cn('transition-colors px-1 py-0.5 text-foreground/80', {
+					'hover:text-foreground/80 text-foreground': isActive,
+				})}
+			>
+				{children}
+			</Link>
+		</li>
+	);
+}
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,3 +1,12 @@
+import NavItem from './nav-item';
+
 export default function Navigation() {
-	return <nav>dsadsadsa</nav>;
+	return (
+		<nav className="flex justify-between items-center h-14 px-6 border-b">
+			<ul className="flex space-x-4 xl:space-x-6">
+				<NavItem href="/">Home</NavItem>
+				<NavItem href="/explore">Explore</NavItem>
+			</ul>
+		</nav>
+	);
 }
