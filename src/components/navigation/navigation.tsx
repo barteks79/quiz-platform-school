9d39945ce2ea82356708f83d903b046f76770bd8
barteks79 <sade79dev@gmail.com>
2025-02-22 19:01:43 +0100

    feat: create SearchBar component

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,14 +1,16 @@
 import NavItem from './nav-item';
 import ThemeSwitcher from '../custom/theme-switcher';
+import SearchBar from '../custom/search-bar';
 
 export default function Navigation() {
 	return (
-		<nav className="flex justify-between items-center h-14 px-6 border-b">
+		<nav className="flex items-center gap-4 h-14 px-6 border-b">
 			<ul className="flex space-x-4 xl:space-x-6">
 				<NavItem href="/">Home</NavItem>
 				<NavItem href="/explore">Explore</NavItem>
 			</ul>
-			<ul>
+			<ul className="flex space-x-1 xl:space-x-2 w-full justify-end">
+				<SearchBar />
 				<ThemeSwitcher />
 			</ul>
 		</nav>
